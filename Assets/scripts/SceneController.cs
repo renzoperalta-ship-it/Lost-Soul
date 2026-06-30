c5ac1d0a879473cace785f8b05b04d4c6205604a
Frances2345 <francesco.moreno@tecsup.edu.pe>
2026-06-30 12:49:52 -0500

    Excuse me, Sir

---
+++ Assets/scripts/SceneController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void TestGame()
+    {
+        SceneManager.LoadScene("BadEnding");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
